1ab1bd332e27f9ee39add005504ea9369208aaf3
unknown <augnik13mondal>
2024-03-16 01:56:12 +0530

    hello

--- demo.c
+++ demo.c
@@ -1,13 +1,10 @@
+#include<stdio.h>
 {
-a=34
-b=12
-c=a*b
-a=&b
-b=&c
-d=&c
-a*b
-b*c
-c*d
-printf("hello world")
-  }
+int min()
+int a=5;
+int *ptr; 
+ptr=&a;
+*ptr=*ptr*5;
+printf("%d",a)
+return 0;
 }
